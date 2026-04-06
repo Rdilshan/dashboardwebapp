011af94a08cec965160de7f825bcc966ad235217
Randika Dilshan <rdilshan077788@gmail.com>
2026-04-06 13:00:59 +0530

    chnage style

--- app/submit-cv/page.tsx
+++ app/submit-cv/page.tsx
@@ -9,8 +9,6 @@ import { StudentLogoutButton } from "../ui/student-logout-button";
 import { BackgroundParticles } from "../ui/background-particles";
 import { SubmitCvForm } from "./submit-cv-form";
 
-
-
 export const metadata: Metadata = {
   title: "Submit Your CV | Industrial Training Portal",
   description:
